ea038075a9999a42df41a21cb368802dc97c0571
Alexandra Rodionova <sascha.rod00@gmail.com>
2021-08-19 12:41:13 +0300

    add solution of the task

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  var currentFloor = 2;
+  var counterUp = $(".counter-up");
+  var floorPath = $(".home-image path");
+  var counterDown = $(".counter-down");
+
+  floorPath.on('mouseover', function () {
+    currentFloor = $(this).attr("data-floor");
+    floorPath.removeClass("current-floor");
+    $(".counter").text(currentFloor);
+  })
+
+  counterUp.on('click', function() {
+    if(currentFloor < 18) {
+      currentFloor++;
+    usCurrentFloor = currentFloor.toLocaleString('en-Us', {minimumIntegerDigits: 2, useGrouping: false});
+    $(".counter").text(usCurrentFloor);
+    floorPath.removeClass("current-floor");
+    $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  })
+
+  counterDown.on('click', function() {
+    if(currentFloor > 1) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString('en-Us', {minimumIntegerDigits: 2, useGrouping: false});
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+    $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  })
+});
